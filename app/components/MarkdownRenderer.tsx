0537f3f38d40f587a2c928f4a1c2096f7292d0e0
drink365 <166272199+drink365@users.noreply.github.com>
2025-08-15 19:13:33 +0800

    Update MarkdownRenderer.tsx

--- app/components/MarkdownRenderer.tsx
+++ app/components/MarkdownRenderer.tsx
@@ -4,10 +4,7 @@ import React from "react"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 
-type Props = {
-  content: string
-  className?: string
-}
+type Props = { content: string; className?: string }
 
 export default function MarkdownRenderer({ content, className }: Props) {
   return (
